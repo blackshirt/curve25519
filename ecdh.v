a37f800b866d86c019ddbbf1f88e3b0e0577fc61
blackshirt <blackshirt@users.noreply.github.com>
2023-08-01 17:20:50 +0700

    Update ecdh.v

--- ecdh.v
+++ ecdh.v
@@ -15,8 +15,8 @@ pub enum Curve {
 	secp256r1 = 0x0017
 	secp384r1 = 0x0018
 	secp521r1 = 0x0019
-	x25519 = 0x001D
-	x448 = 0x001E
+	x25519    = 0x001D
+	x448      = 0x001E
 	ffdhe2048 = 0x0100
 	ffdhe3072 = 0x0101
 	ffdhe4096 = 0x0102
